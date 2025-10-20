0cc16c3d9c5fa7714f9ffdc5874f3b935282a37b
Ivan Netkachev <netkacev@wiseadvice.ru>
2025-10-21 07:52:43 +0800

    docs: configure github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/1c-bsl-conversion-rules-viewer/',
       server: {
         port: 3000,
         host: '0.0.0.0',
